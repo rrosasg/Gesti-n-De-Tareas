4d517866de608b8a1676f65b34a08730a07f3230
CarlosRosas <rrosasgranda@gmail.com>
2026-04-27 11:40:42 +0200

    Añado lista de tareas

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
+import java.util.ArrayList;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         System.out.println("Bienvenido al gestor de tareas");
+
+        ArrayList<Task> listaDeTareas = new ArrayList<>();
     }
 }
